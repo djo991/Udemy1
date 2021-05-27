dca0764b4d21a44f4b7e044ed094edd79e644ee4
Djordje Stojanovic <djordje.stojanovic.nis@gmail.com>
2021-05-27 20:40:35 +0200

    Second commit

--- main.js
+++ main.js
@@ -1,33 +1,44 @@
-var names = ["Dusan", "Neemo", "Milena", "Ana", "Luka", "Dusanka"];
+// var names = ["Dusan", "Neemo", "Milena", "Ana", "Luka", "Dusanka"];
+//
+// for (let n in names) {
+//     if (n.length == 5) {
+//         console.log("The length of the name ", names[n], " is 5")
+//     } else if (n.length < 5){
+//         console.log("The length of the name ", names[n], " is ", names[n].length)
+//     } else {
+//         console.log("The length of the name ", names[n], " is ", names[n].length)
+//     }
+// }
+//
+// function printName(name) {
+//     console.log("My name is ", name);
+// }
 
-for (let n in names) {
-    if (n.length == 5) {
-        console.log("The length of the name ", names[n], " is 5")
-    } else if (n.length < 5){
-        console.log("The length of the name ", names[n], " is ", names[n].length)
-    } else {
-        console.log("The length of the name ", names[n], " is ", names[n].length)
-    }
-}
-
-function printName(name) {
-    console.log("My name is ", name);
-}
-function buttonClicked(){
-    x+=1;
-    if (x<=1) {
-        console.log("Button has been clicked", x,"time.");
-        x=this.x;
-    } else  {
-        console.log("Button has been clicked", x,"times.");
-        x=this.x;
-    }
-}
-
-printName("George");
 var btn = document.getElementById("go-button");
-
 var x = 0;
 
 // DOES NOT WORK FOR X>1 , WORKS WITH ONCLICK
-// btn.addEventListener("click", buttonClicked);
+btn.addEventListener("click", buttonClicked);
+
+function buttonClicked(){
+    x++;
+    if (x===1) {
+        console.log("Button has been clicked", x,"time.");
+        document.getElementById("header1").innerHTML = "You've got a pupper!";
+        document.getElementById("text").innerHTML = "It really is the best news ever!";
+    } else if (x===50) {
+        document.getElementById("text").innerHTML = "Are you really not tired of clicking?";
+        document.getElementById("go-button").innerHTML = "No!!!";
+        console.log("Button has been clicked", x, "times.");
+    } else if (x===51){
+        document.getElementById("text").innerHTML = "It really is the best news ever!";
+        document.getElementById("go-button").innerHTML = "Yeah!!!";
+        console.log("Button has been clicked", x, "times.");
+    } else if (x===100) {
+        document.getElementById("text").innerHTML = "You really have the energy for 100 people!";
+        document.getElementById("go-button").innerHTML = "Hell yeah!!!";
+        console.log("Button has been clicked", x, "times.");
+    } else console.log("Button has been clicked", x, "times.");
+
+}
+
