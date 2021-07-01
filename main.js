95e02b118a8d68d7060f2cc8e63646ee265688d7
Djordje Stojanovic <djordje.stojanovic.nis@gmail.com>
2021-07-01 20:45:07 +0200

    Second commit

--- main.js
+++ main.js
@@ -1,44 +1,31 @@
-// var names = ["Dusan", "Neemo", "Milena", "Ana", "Luka", "Dusanka"];
-//
-// for (let n in names) {
-//     if (n.length == 5) {
-//         console.log("The length of the name ", names[n], " is 5")
-//     } else if (n.length < 5){
-//         console.log("The length of the name ", names[n], " is ", names[n].length)
-//     } else {
-//         console.log("The length of the name ", names[n], " is ", names[n].length)
-//     }
-// }
-//
-// function printName(name) {
-//     console.log("My name is ", name);
-// }
-
+var nums = new Array();
 var btn = document.getElementById("go-button");
-var x = 0;
+var text = document.getElementById("myinput");
+btn.addEventListener("click",findMissingNo);
+function convertNums(nums) {
 
-// DOES NOT WORK FOR X>1 , WORKS WITH ONCLICK
-btn.addEventListener("click", buttonClicked);
+    var nums = text.value;
+    nums = nums.trim().split(',');
+    nums.forEach(element => parseInt(element));
+    return nums;
 
-function buttonClicked(){
-    x++;
-    if (x===1) {
-        console.log("Button has been clicked", x,"time.");
-        document.getElementById("header1").innerHTML = "You've got a pupper!";
-        document.getElementById("text").innerHTML = "It really is the best news ever!";
-    } else if (x===50) {
-        document.getElementById("text").innerHTML = "Are you really not tired of clicking?";
-        document.getElementById("go-button").innerHTML = "No!!!";
-        console.log("Button has been clicked", x, "times.");
-    } else if (x===51){
-        document.getElementById("text").innerHTML = "It really is the best news ever!";
-        document.getElementById("go-button").innerHTML = "Yeah!!!";
-        console.log("Button has been clicked", x, "times.");
-    } else if (x===100) {
-        document.getElementById("text").innerHTML = "You really have the energy for 100 people!";
-        document.getElementById("go-button").innerHTML = "Hell yeah!!!";
-        console.log("Button has been clicked", x, "times.");
-    } else console.log("Button has been clicked", x, "times.");
+}
 
+function findMissingNo () {
+    var nums = text.value;
+    nums = nums.trim().split(',');
+    nums.forEach(element => parseInt(element));
+    nums = nums.sort((a, b) => (a - b));
+    console.log(nums);
+    for (var i = 0; i < nums.length; i++) {
+        if (i != nums.length - 1) {
+            if (nums[i + 1] - nums[i] != 1) {
+                for (var m = 1; m < (nums[i + 1] - nums[i]); m++) {
+                    var x = parseInt(nums[i]) + parseInt(m);
+                    console.log("The missing number is: " + x)
+                }
+            }
+        }
+    }
 }
 
